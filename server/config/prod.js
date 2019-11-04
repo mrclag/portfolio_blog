79150a2e8d9846eb3065628a9e6d4e125b62552c
mrclag <mattrclagett@gmail.com>
2019-11-03 21:02:10 -0800

    needed for dev build

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,5 @@
 module.exports = {
-  DB_URI: process.env.DB_URI
+  DB_URI:
+    process.env.DB_URI ||
+    'mongodb+srv://mattclagett:testabc123@portfolio-weay6.mongodb.net/test?retryWrites=true&w=majority'
 };
