aa39641d0ad46349986b3de9cfeb587ff323be4a
mrclag <mattrclagett@gmail.com>
2019-11-04 14:07:20 -0800

    fix env variable of namepace on server & axios base url on client

--- server/services/auth.js
+++ server/services/auth.js
@@ -1,5 +1,8 @@
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
+const config = require('../config');
+
+const NAMESPACE = config.NAMESPACE;
 
 // Middleware
 exports.checkJWT = jwt({
@@ -17,7 +20,7 @@ exports.checkJWT = jwt({
 exports.checkRole = role => (req, res, next) => {
   const user = req.user;
 
-  if (user && user[process.env.NAMESPACE + '/role'] === role) {
+  if (user && user[NAMESPACE + '/role'] && user[NAMESPACE + '/role'] === role) {
     next();
   } else {
     return res.status(401).send({
