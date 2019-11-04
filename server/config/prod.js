69eabb7f75ce566319030a81e0c2170b0c73af7e
mrclag <mattrclagett@gmail.com>
2019-11-04 13:05:26 -0800

    only prod db_ui

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,3 @@
 module.exports = {
-  DB_URI:
-    process.env.DB_URI ||
-    'mongodb+srv://mattclagett:testabc123@portfolio-weay6.mongodb.net/test?retryWrites=true&w=majority'
+  DB_URI: process.env.DB_URI
 };
