b30e5de70aff2163f5fbfa51a2a521d852b5b055
mrclag <mattrclagett@gmail.com>
2019-12-05 19:25:44 -0800

    converted project modals to use react/hooks rather than bootstrap

--- components/portfolios/PortfolioCard.js
+++ components/portfolios/PortfolioCard.js
@@ -3,45 +3,63 @@ import PortfolioCardDetail from './PortfolioCardDetail';
 import { Card, CardBody, CardText, CardTitle, CardImg } from 'reactstrap';
 import ButtonLink from '../ButtonLink';
 
+import Modal from './modal';
+
 const PortfolioCard = props => {
   const [open, setOpen] = useState(false);
+  const [isModalOpen, toggleModal] = useState(false);
   const { portfolio, children } = props;
 
   const toggle = () => setOpen(!open);
 
   return (
-    <span onClick={toggle}>
-      <PortfolioCardDetail
-        toggle={toggle}
-        portfolio={portfolio}
-        isOpen={open}
-      />
-      <Card className="portfolio-card">
-        <CardImg
-          style={{
-            width: '100%',
-            height: '15vw',
-            objectFit: 'cover'
-          }}
-          src={portfolio.imageUrl}
-          alt="Card image"
-          className="portfolio-card-img"
+    <>
+      <Modal isOpen={isModalOpen} toggle={toggleModal}>
+        <img src={portfolio.imageUrl} style={{ width: '100%' }} alt="" />
+        <h1 style={{ margin: '10px 0px' }}>
+          <b>{portfolio.title}</b>
+        </h1>
+        <p>
+          <b>Description: </b>
+          {portfolio.description}
+        </p>
+        <ButtonLink link={portfolio.githubUrl} icon="github" />
+        <ButtonLink link="https://github.com/mrclag" icon="home" />
+        <button onClick={() => toggleModal(false)}>Close</button>
+      </Modal>
+      <span onClick={() => toggleModal(!isModalOpen)}>
+        <PortfolioCardDetail
+          toggle={toggle}
+          portfolio={portfolio}
+          isOpen={open}
         />
+        <Card className="portfolio-card">
+          <CardImg
+            style={{
+              width: '100%',
+              height: '15vw',
+              objectFit: 'cover'
+            }}
+            src={portfolio.imageUrl}
+            alt="Card image"
+            className="portfolio-card-img"
+          />
 
-        <CardBody>
-          <CardTitle className="portfolio-card-title">
-            {portfolio.title}
-          </CardTitle>
-          <CardText
-            className="portfolio-card-text"
-            style={{ minHeight: '5vw' }}
-          >
-            {portfolio.blurb}{' '}
-          </CardText>{' '}
-          <div className="readMore">{children}</div>
-        </CardBody>
-      </Card>
-    </span>
+          <CardBody>
+            <CardTitle className="portfolio-card-title">
+              {portfolio.title}
+            </CardTitle>
+            <CardText
+              className="portfolio-card-text"
+              style={{ minHeight: '5vw' }}
+            >
+              {portfolio.blurb}{' '}
+            </CardText>{' '}
+            <div className="readMore">{children}</div>
+          </CardBody>
+        </Card>
+      </span>
+    </>
   );
 };
 
